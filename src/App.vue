dd6f1344fd4151b1d122c21a7c68a0a6c24f5c97
Karthik K Devadiga <kdevadiga497@gmail.com>
2022-01-09 13:12:29 +0530

    Added css file | added icons and images | created separete components for Header

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,7 @@
 <!-- prettier-ignore -->
 <template>
-<header id="header" class="bg-gray-700">
-    <nav class="container mx-auto flex justify-start items-center py-5 px-4">
-      <!-- App Name -->
-      <a class="text-white font-bold uppercase text-2xl mr-4" href="#">Music</a>
-
-      <div class="flex flex-grow items-center">
-        <!-- Primary Navigation -->
-        <ul class="flex flex-row mt-1">
-          <!-- Navigation Links -->
-          <li>
-            <a class="px-2 text-white" href="#">Login / Register</a>
-          </li>
-          <li>
-            <a class="px-2 text-white" href="#">Manage</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </header>
-
+  <!-- Header -->
+  <app-header></app-header>
   <!-- Introduction -->
   <section class="mb-8 py-20 text-white text-center relative">
     <div class="absolute inset-0 w-full h-full bg-contain introduction-bg"
@@ -38,7 +20,7 @@
     </div>
 
     <img class="relative block mx-auto mt-5 -mb-20 w-auto max-w-full"
-      src="assets/img/introduction-music.png" />
+      src="./assets/img/introduction-music.png" />
   </section>
 
   <!-- Main Content -->
@@ -404,3 +386,15 @@
     </div>
   </div>
 </template>
+
+<script>
+import './assets/img/introduction-music.png';
+import AppHeader from './components/Header.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
+// prettier-ignore
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './assets/tailwind.css';
+import './assets/main.css';
+import './assets/img/header.png';
 
 createApp(App).use(store).use(router).mount('#app');
