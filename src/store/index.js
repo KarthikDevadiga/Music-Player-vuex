2fe74e4854d261e22ccf8531137a155575256c8c
Karthik K Devadiga <kdevadiga8055@gmail.com>
2022-01-30 13:28:41 +0530

    c-f-a
    linked auth uid to firestore user uid

--- src/store/index.js
+++ src/store/index.js
@@ -17,15 +17,20 @@ export default createStore({
   },
   actions: {
     async register({ commit }, values) {
-      await auth.createUserWithEmailAndPassword(values.email, values.password); // if user is not authenticated this will set to null
+      const userCred = await auth.createUserWithEmailAndPassword(values.email, values.password); // if user is not authenticated this will set to null
       // storing in fireStore (DB)
-      await userData.add({
+      await userData.doc(userCred.user.uid).set({
+        // sets the userid generated from auth
         // returns a promise
         name: values.name,
         email: values.email,
         age: values.age,
         country: values.country,
         role: values.purpose,
+      });
+      // storing additiona information but it is limited. we can only store display name and profile image
+      userCred.user.updateProfile({
+        displayName: values.name,
       });
       commit('TOGGLE_USER_LOGEDIN'); // can pass other value (payload) => this.$store.dispatch('tooglelogin', value);
     },
