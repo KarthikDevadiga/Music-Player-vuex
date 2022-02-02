7a3fd39bf578f17d585974eb74432f530d7036b4
Karthik K Devadiga <kdevadiga8055@gmail.com>
2022-02-02 11:37:50 +0530

    routing
    initial setup 2

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
-];
+const routes = [];
 
 const router = createRouter({
   // function returning an object
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(process.env.BASE_URL), // cofiguration setting
+  // process => node object globally avilable
+  // env => enviornment
+  // baseurl can be changed through vue cli
   routes,
 });
 
